832356e2087e2981b00e4152fff5b2b1a374d8ac
guillermoSb <guillermo.santosba@gmail.com>
2021-07-20 07:42:17 -0600

    Iniciando proyecto FatCat

---
+++ fatcat/MyCat.java
@@ -0,0 +1,46 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * MyCat is your own cat. Get it to do things by writing code in its act method.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyCat extends Cat
+{
+    /**
+     * Act - do whatever the MyCat wants to do.
+     */
+    public void act()
+    {
+        // Cada uno de los comentarios
+        
+        // Dormir si el gato está cansado
+        if (isSleepy()) {
+            sleep(10);
+            shoutHooray();
+        } else {
+            shoutHooray();
+        }
+        // Bailar si el gato esta aburrido
+        if (isBored()) {
+            dance();
+        }
+        // Comer si tiene hambre
+        if(isHungry()) {
+            eat();
+        }
+        
+        if (isAlone()) {
+            sleep(10);
+        } else {
+            shoutHooray();
+        }
+        //eat();
+        //dance();
+        //sleep(10);
+        //eat();
+        //eat();
+        //dance();
+    }    
+}
